50b2528615f5015e017f9a674f772bf6f85e81fb
Eduardo Salazar <german2506.gs@gmail.com>
2019-09-26 17:52:53 -0600

    missing upload presset

--- src/components/ingredients/ingredient_form_hoc.jsx
+++ src/components/ingredients/ingredient_form_hoc.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import IngredientForm from './ingredient_form';
-import RootRef from '@material-ui/core/RootRef'
+import axios from "axios";
+import cloudinary from 'cloudinary-core'; 
 
 export class IngredientFormHOC extends Component {
   constructor(props) {
@@ -68,10 +69,76 @@ export class IngredientFormHOC extends Component {
       this.setState({
         validated: false,
       })
+
     } else {
-      this.props.create_ingredient(this.state.ingredient)
+      //let cl = new cloudinary.Cloudinary({cloud_name: "demo", secure: true});
+
+      let url = `https://api.cloudinary.com/v1_1/dbo96sjb/upload`;
+      let fd = new FormData();
+      // fd.append("upload_preset", unsignedUploadPreset);
+      fd.append("tags", "browser_upload"); // Optional - add tag for image admin in Cloudinary
+      fd.append("upload_preset", "demo preset")
+      fd.append("api_key", "757447362712211");
+      fd.append("api_secret", "z_F0g_ccUUJG24DDJJjyNdjl0RM");
+      fd.append("file", image);
+      const config = {}
+      axios.post(url, fd, config)
+              .then(function (res) {
+                console.log(res);
+                this.props.create_ingredient(this.state.ingredient)
+              })
+              .catch(function (err) {
+                console.error('err', err);
+              });
+
+
+
+
     }
   }
+
+  /*function uploadFile(file) {
+    var url = `https://api.cloudinary.com/v1_1/dbo96sjb/upload`;
+    var fd = new FormData();
+    // fd.append("upload_preset", unsignedUploadPreset);
+    fd.append("tags", "browser_upload"); // Optional - add tag for image admin in Cloudinary
+    fd.append("api_key", "757447362712211");
+    fp.append("api_secret", "z_F0g_ccUUJG24DDJJjyNdjl0RM");
+    fd.append("file", file);
+    const config = {}
+      /*headers: { "X-Requested-With": "XMLHttpRequest" },
+      onUploadProgress: function(progressEvent) {
+        // Do whatever you want with the native progress event
+        // console.log('progressEvent', progressEvent);
+        var progress = Math.round((progressEvent.loaded * 100.0) / progressEvent.total);
+        document.getElementById('progress').style.width = progress + "%";
+  
+        console.log(`onUploadProgress progressEvent.loaded: ${progressEvent.loaded},
+      progressEvent.total: ${progressEvent.total}`);
+      }
+    };
+    axios.post(url, fd, config)
+              .then(function (res) {
+                // console.log('res', res)
+                // File uploaded successfully
+                /*var response = res.data;
+                // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
+                var url = response.secure_url;
+                // Create a thumbnail of the uploaded image, with 150px width
+                var tokens = url.split('/');
+                tokens.splice(-2, 0, 'w_150,c_scale');
+                var img = new Image(); // HTML5 Constructor
+                img.src = tokens.join('/');
+                img.alt = response.public_id;
+                document.getElementById('gallery').appendChild(img);
+              })
+              .catch(function (err) {
+                console.error('err', err);
+              });
+  
+    // Reset the upload progress bar
+    document.getElementById("progress").style.width = 0;
+  }*/
 
 
 
