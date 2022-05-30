1e4e53951d4625d6e771aa60258450adf6722eac
German Eduardo Salazar Aranda <german2506.gs@gmail.com>
2022-05-30 13:19:20 -0600

    changed requests into one lines

--- src/services/measure_request.js
+++ src/services/measure_request.js
@@ -4,41 +4,12 @@ import { apiDelete } from "./foodsharebook_api"
 
 const path = 'measures'
 
-export const getMeasure = id => {
-    return getAndDispatch({
-        path: `${path}/${id}`,
-        action: setMeasure
-    })
-}
+export const getMeasure = id => getAndDispatch({ path: `${path}/${id}`, action: setMeasure })
 
-export const getMeasures = params => {
-    return getAndDispatch({
-        path,
-        action: setMeasures,
-        params: { page: 1, per_page: 10, ...params }
-    })
-}
+export const getMeasures = params => getAndDispatch({ path, action: setMeasures, params: { page: 1, per_page: 10, ...params } })
 
-export const postMeasure = data => {
-    return postAndDispatch({
-        path,
-        action: addMeasure,
-        data
-    })
-}
+export const postMeasure = data => postAndDispatch({ path, action: addMeasure, data })
 
-export const putMeasure = params => {
-    return putAndDispatch({
-        path,
-        id: params.id,
-        data: params.data,
-        action: addMeasure
-    })
-}
+export const putMeasure = params => putAndDispatch({ path, id: params.id, data: params.data, action: addMeasure })
 
-export const deleteMeasure = (id) => {
-    return apiDelete({
-        path,
-        id
-    })
-}
+export const deleteMeasure = id => apiDelete({ path, id })
